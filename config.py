ed2986d230349631324959b753ababbd2d7f21a4
caiseyann <caiseyann4@gmail.com>
2020-09-12 12:45:10 +0300

    created a new shell file

--- config.py
+++ config.py
@@ -1,9 +1,34 @@
-from app import create_app
-from flask_script import Manager, Server
+import os
 
-app = create_app('development')
 
-manager = Manager(app)
-manager.add_command('server', Server)
-if __name__ == '__main__':
-    manager.run()
+class Config:
+    """
+    General config parent class
+    """
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+    ARTICLE_URL = 'https://newsapi.org/v2/everything?q=trending&language=en&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
+
+class ProdConfig(Config):
+    """
+    Production configuration child class
+    Args:
+      Config : The parent configuration class with general configuration settings
+    """
+    pass
+
+
+class DevConfig(Config):
+    """
+    Development configuration child class
+    Args:
+      Config: The parent configuration class with general configuration settings
+    """
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
