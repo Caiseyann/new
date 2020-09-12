91cc29020cad060af27452ed6fc05721e0adc095
caiseyann <caiseyann4@gmail.com>
2020-09-12 12:14:09 +0300

    added class ProdConfig

--- app/request.py
+++ app/request.py
@@ -8,3 +8,12 @@ class Config:
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
     ARTICLE_URL = 'https://newsapi.org/v2/everything?q=trending&language=en&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
+
+class ProdConfig(Config):
+    """
+    Production configuration child class
+    Args:
+    Config : The parent configuration class with general configuration settings
+    """
+    pass
