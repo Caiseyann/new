8e7a59dd31f100f5d84e511b18424d464352a5ef
caiseyann <caiseyann4@gmail.com>
2020-09-12 11:52:13 +0300

    class Source in models.py created defining __init__(self)

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,14 @@
 from flask import Flask
-from flask_bootstrap import Boot
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    app.config.from_object(config_options[config_name])
+
+
+    bootstrap.init_app(app)
+
+   return app
---
+++ app/models.py
@@ -0,0 +1,9 @@
+class Source:
+    """
+    class will define news objects
+    """
+    def __init__(self):
+        self.id = id
+
+
+
