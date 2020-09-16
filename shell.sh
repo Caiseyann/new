c95227301453ed9122c2ff5d7b7114baa76452b1
caiseyann <caiseyann4@gmail.com>
2020-09-16 10:52:00 +0300

    updated secret key

--- shell.sh
+++ shell.sh
@@ -1,4 +1,5 @@
-NEWS_API_KEY= '84e7c870c01546f88628446e7f2da327'
-SECRET_API_KEY= '84e7c870c01546f88628446e7f2da327'
+export NEWS_API_KEY= '84e7c870c01546f88628446e7f2da327'
+export SECRET_KEY='Flask WTF Secret Key'
+
 
 python manage.py server
